e7991b4d8a981542311b3f27411d3a287de3336d
JarWarren <gentlemanjaruna@gmail.com>
2020-07-26 14:51:06 -0600

    m to mute

--- Singletons/BackgroundMusicPlayer.gd
+++ Singletons/BackgroundMusicPlayer.gd
@@ -4,6 +4,9 @@ extends AudioStreamPlayer
 const OVERWORLD_THEME = preload("res://Audio/Blue_Dot_Sessions_-_02_-_Rabbit_Hole.ogg")
 const UNDERWORLD_THEME = preload("res://Audio/SPCZ_-_10_-_Ground.ogg")
 
+# properties
+var paused = false
+var bookmark = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,14 +14,31 @@ func _ready():
 	stream = OVERWORLD_THEME
 	play()
 
+func _process(_delta):
+	if Input.is_action_just_pressed("audio_mute"):
+		match paused:
+			true:
+				play(bookmark)
+				paused = false
+			false:
+				bookmark = get_playback_position()
+				stop()
+				paused = true
+
 
 func change_to_overworld():
 	if stream != OVERWORLD_THEME:
 		stream = OVERWORLD_THEME
-		play()
+		_play()
 
 
 func change_to_underworld():
 	if stream != UNDERWORLD_THEME:
 		stream = UNDERWORLD_THEME
+		_play()
+
+
+func _play():
+	bookmark = 0
+	if not paused:
 		play()
