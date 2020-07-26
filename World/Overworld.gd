3f575539aa9b880bd3adf58b29c3b241a3921fca
JarWarren <gentlemanjaruna@gmail.com>
2020-07-26 03:10:08 -0600

    add reaper easter egg

--- World/Overworld.gd
+++ World/Overworld.gd
@@ -5,12 +5,15 @@ extends Node
 onready var rabbit = $Rabbit
 onready var camera = $Rabbit/Camera2D
 onready var light = $Rabbit/Light2D
+onready var decoration = $Decoration
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	camera.current = false
 	light.enabled = false
+	if GameController.deaths >= 20:
+		decoration.texture = load("res://Sprites/reaper.png")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
